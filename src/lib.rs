50e9cc3580ca0b05b4a21bb8c31474398f9cd6e8
Jacob Asper <jacobasper191@gmail.com>
2024-02-11 04:47:54 -0500

    add description to Gcg struct and test

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,8 @@ use player::Player;
 pub struct Gcg {
 	pub player1: Player,
 	pub player2: Player,
+	/// A long description of the game. May contain HTML entities
+	pub description: Option<String>,
 }
 
 impl Gcg {
@@ -49,6 +51,7 @@ impl Gcg {
 			player2: player2.ok_or_else(|| GcgError::MissingPragma {
 				keyword: "player2".to_string(),
 			})?,
+			description: None,
 		};
 
 		Ok(gcg)
@@ -71,17 +74,18 @@ mod tests {
 		let gcg = Gcg::build(&text)?;
 
 		assert_eq!(
-			gcg,
-			Gcg {
-				player1: Player {
-					nickname: "20jasper".to_string(),
-					full_name: "Jacob Asper".to_string(),
-				},
-				player2: Player {
-					nickname: "xXFerrisXx".to_string(),
-					full_name: "Ferris The Crab".to_string(),
-				},
-			}
+			gcg.player1,
+			Player {
+				nickname: "20jasper".to_string(),
+				full_name: "Jacob Asper".to_string(),
+			},
+		);
+		assert_eq!(
+			gcg.player2,
+			Player {
+				nickname: "xXFerrisXx".to_string(),
+				full_name: "Ferris The Crab".to_string(),
+			},
 		);
 
 		Ok(())
@@ -110,5 +114,21 @@ mod tests {
 
 		assert!(error.contains("unknown pragma"));
 		assert!(error.contains("#whatisthispragma"));
+	}
+
+	#[test]
+	fn should_parse_description() -> Result<()> {
+		let text = [
+			"#player1 20jasper Jacob Asper",
+			"#player2 xXFerrisXx Ferris The Crab",
+			"#description 20jasper vs xXFerrisXx",
+		]
+		.join("\n");
+
+		let gcg = Gcg::build(&text)?;
+
+		assert_eq!(gcg.description, Some("20jasper vs xXFerrisXx".to_string()));
+
+		Ok(())
 	}
 }
