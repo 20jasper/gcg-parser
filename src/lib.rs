201f6d0432801a243703156cfc0843da9108142a
Jacob Asper <78604367+20jasper@users.noreply.github.com>
2024-02-11 05:57:53 -0500

    feat!: parse #description pragma
    BREAKING CHANGE: add `description` field to `Gcg` struct

--- src/lib.rs
+++ src/lib.rs
@@ -20,12 +20,15 @@ use player::Player;
 pub struct Gcg {
 	pub player1: Player,
 	pub player2: Player,
+	/// A long description of the game. May contain HTML entities
+	pub description: Option<String>,
 }
 
 impl Gcg {
 	pub fn build(text: &str) -> Result<Gcg> {
 		let mut player1 = None::<Player>;
 		let mut player2 = None::<Player>;
+		let mut description = None::<String>;
 
 		for (i, line) in text.lines().enumerate() {
 			if line.starts_with("#player1") {
@@ -34,6 +37,10 @@ impl Gcg {
 			} else if line.starts_with("#player2") {
 				let player = Player::build(line, i)?;
 				player2 = Some(player);
+			} else if line.starts_with("#description") {
+				let (_, desc) = line.split_once(' ').unwrap_or_default();
+
+				description = Some(desc.to_string());
 			} else {
 				return Err(GcgError::UnknownPragma {
 					line: text.to_string(),
@@ -49,6 +56,7 @@ impl Gcg {
 			player2: player2.ok_or_else(|| GcgError::MissingPragma {
 				keyword: "player2".to_string(),
 			})?,
+			description,
 		};
 
 		Ok(gcg)
@@ -71,17 +79,18 @@ mod tests {
 		let gcg = Gcg::build(&text)?;
 
 		assert_eq!(
-			gcg,
-			Gcg {
-				player1: Player {
-					nickname: "20jasper".to_string(),
-					full_name: "Jacob Asper".to_string(),
-				},
-				player2: Player {
-					nickname: "xXFerrisXx".to_string(),
-					full_name: "Ferris The Crab".to_string(),
-				},
-			}
+			gcg.player1,
+			Player {
+				nickname: "20jasper".to_string(),
+				full_name: "Jacob Asper".to_string(),
+			},
+		);
+		assert_eq!(
+			gcg.player2,
+			Player {
+				nickname: "xXFerrisXx".to_string(),
+				full_name: "Ferris The Crab".to_string(),
+			},
 		);
 
 		Ok(())
@@ -110,5 +119,21 @@ mod tests {
 
 		assert!(error.contains("unknown pragma"));
 		assert!(error.contains("#whatisthispragma"));
+	}
+
+	#[test]
+	fn should_parse_description() -> Result<()> {
+		let text = [
+			"#player1 20jasper Jacob Asper",
+			"#player2 xXFerrisXx Ferris The Crab",
+			"#description 20jasper vs xXFerrisXx",
+		]
+		.join("\n");
+
+		let gcg = Gcg::build(&text)?;
+
+		assert_eq!(gcg.description, Some("20jasper vs xXFerrisXx".to_string()));
+
+		Ok(())
 	}
 }
